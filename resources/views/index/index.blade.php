7b0c5bdde249fe3b1e29291794cf8d0d114264a9
chunge <184364921@qq.com>
2018-02-03 18:59:50 +0800

    首页图片轮动预加载

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -75,7 +75,7 @@
     $(".lazy").lazyload({
         event: "scrollstop",
 		effect : "fadeIn",
-        container: $(".flexitemv.box"),
+        container: $(".listbox"),
         load:function ($e) {
             $e.css({"width":"100%","height":"100%"});
         }
